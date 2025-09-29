e0749403543ec51023c81b9425afc9c03ad824f4
Bammrie <126354380+Bammrie@users.noreply.github.com>
2025-09-29 12:58:02 -0500

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,23 @@
+// Editable config for game objects
+const gameObjects = [
+  { id: 1, name: "Base", image: "images/Object0-1.png", reward: 0.0000000100, chance: 60 },
+  { id: 2, name: "Blue", image: "images/Object0-2.png", reward: 0.0000000300, chance: 20 },
+  { id: 3, name: "Red", image: "images/Object0-3.png", reward: 0.0000000500, chance: 6 },
+  { id: 4, name: "Gold", image: "images/Object0-4.png", reward: 0.0000001000, chance: 4 },
+  { id: 5, name: "Rainbow", image: "images/Object0-5.png", reward: 0.0000025000, chance: 0.0 }
+  { id: 6, name: "Rainbow", image: "images/Object0-6.png", reward: 0.0000025000, chance: 0.0 }
+  { id: 7, name: "Base", image: "images/Object0-7.png", reward: 0.0000000100, chance: 0.0 },
+  { id: 8, name: "Blue", image: "images/Object0-8.png", reward: 0.0000000300, chance: 0.0 },
+  { id: 9, name: "Red", image: "images/Object0-9.png", reward: 0.0000000500, chance: 0.0 },
+  { id: 10, name: "Gold", image: "images/Object1-0.png", reward: 0.0000001000, chance: 3 },
+  { id: 11, name: "Rainbow", image: "images/Object1-1.png", reward: 0.0000025000, chance: 2 }
+  { id: 12, name: "Rainbow", image: "images/Object1-2.png", reward: 0.0000025000, chance: 1.5 }
+  { id: 13, name: "Base", image: "images/Object1-3.png", reward: 0.0000000100, chance: 1 },
+  { id: 14, name: "Blue", image: "images/Object1-4.png", reward: 0.0000000300, chance: .5 },
+  { id: 15, name: "Red", image: "images/Object1-5.png", reward: 0.0000000500, chance: 0 },
+  { id: 16, name: "Gold", image: "images/Object1-6.png", reward: 0.0000001000, chance: 0.0 },
+  { id: 17, name: "Rainbow", image: "images/Object1-7.png", reward: 0.0000025000, chance: 0.0 }
+  { id: 18, name: "Rainbow", image: "images/Object1-8.png", reward: 0.0000025000, chance: 0.0 }
+  { id: 19, name: "Gold", image: "images/Object1-9.png", reward: 0.0000001000, chance: 0.0 },
+  { id: 20, name: "Rainbow", image: "images/Object2-0.png", reward: 0.0000025000, chance: 2.0 }
+];
