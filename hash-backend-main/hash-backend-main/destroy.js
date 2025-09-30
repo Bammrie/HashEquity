f8e77e07bd3e69237e5a707c2374bdc3233d6130
Bammrie <126354380+Bammrie@users.noreply.github.com>
2025-09-30 17:51:08 -0500

    Delete hash-backend-main/hash-backend-main directory

--- hash-backend-main/hash-backend-main/destroy.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Temporary placeholder route
-router.post("/", (req, res) => {
-  res.json({ message: "Destroy route works!" });
-});
-
-module.exports = router;
